c33a26c71b67b4eca65ca996ffc0079ce0b59524
SebastianBessoudo <158110649+SebastianBessoudo@users.noreply.github.com>
2024-03-19 19:04:32 -0700

    doxygen fixed?

--- docs/search/functions_7.js
+++ docs/search/functions_7.js
@@ -1,6 +1,7 @@
 var searchData=
 [
-  ['task2_5fmotorcontrol_0',['task2_motorcontrol',['../main_8py.html#a04dca862672db1106c56761401a0de28',1,'main']]],
-  ['task3_5fcamera_1',['task3_camera',['../main_8py.html#a3df0baa2e0571edb53b29bddc04ba304',1,'main']]],
-  ['test_5fmlx_5fcam_2',['test_MLX_cam',['../mlx__cam_8py.html#a412d461b290a706127bf8dd299b0635f',1,'mlx_cam']]]
+  ['task1_5fmm_0',['task1_mm',['../main_8py.html#ab360a11f6bf7e0cbac917b4d2e422e91',1,'main']]],
+  ['task2_5fmotorcontrol_1',['task2_motorcontrol',['../main_8py.html#a04dca862672db1106c56761401a0de28',1,'main']]],
+  ['task3_5fcamera_2',['task3_camera',['../main_8py.html#a3df0baa2e0571edb53b29bddc04ba304',1,'main']]],
+  ['test_5fmlx_5fcam_3',['test_MLX_cam',['../mlx__cam_8py.html#a412d461b290a706127bf8dd299b0635f',1,'mlx_cam']]]
 ];
